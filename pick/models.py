a6375fff6805df99430440c8d03467e71ea5f977
JU <chris34651@gmail.com>
2023-01-07 11:48:47 +0800

    第一家店和飲料

--- pick/models.py
+++ pick/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Shop(models.Model):
+    #店家名稱
+    shopname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id) + ")" + self.shopname
+    
+class Option(models.Model):
+    #屬於哪一家店
+    shop_id = models.IntegerField()
+    #選項文字
+    drinkname = models.CharField(max_length=50)
+    #被訂購數
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.shop_id) + "： " + self.drinkname
+        
--- pick/views.py
+++ pick/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, RedirectView, CreateView, UpdateView, DeleteView
+from .models import *
 
 # Create your views here.
+
+#主畫面
